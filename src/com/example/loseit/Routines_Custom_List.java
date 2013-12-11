bd2d90d4d67bdc679fea75825560e2e89d5b3827
green07 <rim_lasallian@yahoo.com>
2013-12-11 23:39:02 +0800

    All changes is done

---
+++ src/com/example/loseit/Routines_Custom_List.java
@@ -0,0 +1,37 @@
+package com.example.loseit;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Routines_Custom_List extends ArrayAdapter<String>{
+	
+	private final Activity context;
+	private final String[] routines;
+	private final Integer[] imageId;
+	//private final Integer[] statusId;
+	
+	public Routines_Custom_List(Activity context, String[] routines, Integer[] imageId){
+		super(context, R.layout.routines_single_list, routines);
+		this.context = context;
+		this.routines = routines;
+		this.imageId = imageId;
+	}
+	
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		LayoutInflater inflater = context.getLayoutInflater();
+		View rowView= inflater.inflate(R.layout.routines_single_list, null, true);
+		TextView txtTitle = (TextView) rowView.findViewById(R.id.routines_txt);
+		 
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.routines_img);
+		txtTitle.setText(routines[position]);
+		 
+		imageView.setImageResource(imageId[position]);
+		return rowView;
+	}
+}
